0ff4ebda92ce882c6a73ace8f1d3da3638edb34b
Rob L <81087205+flashrob01@users.noreply.github.com>
2022-11-21 23:59:50 +0800

    .
    .

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import Tour from "./pages/Tour";
 import {
   BottomNavigationAction,
   Box,
@@ -39,7 +38,6 @@ function App() {
         <SearchAppBar />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/:id" element={<Tour />} />
         </Routes>
         <BottomNavigation>
           <BottomNavigationAction>
